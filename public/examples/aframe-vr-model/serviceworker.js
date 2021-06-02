9f9cb3ea50de79a0b4f4a4519abc158d87ac2f88
Alex Gill <alex.gill@theuniprogroup.com>
2021-06-02 12:05:27 +0100

    Add Aframe as local file to simplify prefetch

--- public/examples/aframe-vr-model/serviceworker.js
+++ public/examples/aframe-vr-model/serviceworker.js
@@ -1,21 +1,14 @@
 var cacheName = "pwa";
 var prefetchUrls = [
   '/examples/aframe-vr-model/',
-  'https://cdn.jsdelivr.net/gh/aframevr/aframe/dist/aframe-master.min.js'
+  '/examples/aframe-vr-model/aframe-master.min.js'
 ];
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(cacheName)
-      .then(function(cache) {
-        console.log('Opened cache');
-        cache.addAll(prefetchUrls.map(function(urlToPrefetch) {
-           return new Request(urlToPrefetch, { mode: 'no-cors' });
-        })).then(function() {
-          console.log('All resources have been fetched and cached.');
-        });
-      })
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(prefetchUrls);
+    })
   );
 });
 
