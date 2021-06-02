4b56b5774bb775cf4ebd6fdc52ddf0313a089f70
Alex Gill <alex.gill@theuniprogroup.com>
2021-06-02 11:23:57 +0100

    Change to express setup

--- README.md
+++ README.md
@@ -1,16 +1,27 @@
 # WebXR examples
+
 These are examples of WebXR.
 
 ## Libraries and Frameworks
+
 - https://ar-js-org.github.io/AR.js-Docs
 - https://aframe.io/docs/1.2.0/introduction
 
 ### Setup
+
 Uses an express.js server over HTTPS to run input and output devices via the browser including cameras, microphones and speakers.
+
+First, create server.key and server.crt files.
+
+```bash
+$ openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./sslcert/server.key -out ./sslcert/server.crt
+```
+
+Then install express package and run.
 
 ```bash
 $ npm install
 $ npm run start
 ```
 
-Open https://localhost:8080 to view.
+Open http://localhost:8080 or https://localhost:8443 to view.
--- index.js
+++ index.js
@@ -1,21 +1,30 @@
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+const certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+const credentials = {key: privateKey, cert: certificate};
 const express = require('express');
-const fs = require('fs')
-const https = require('https')
-const path = require('path');
 const app = express();
-
-app.set('port', (process.env.PORT || 8080));
+const cors = require('cors');
 
 app.use(express.static('public'));
+app.use(cors({origin: 'null'}));
 
-https.createServer({
-  key: fs.readFileSync('server.key'),
-  cert: fs.readFileSync('server.cert')
-}, app)
-.listen(app.get('port'), function(err) {
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(8080, function(err) {
   if (err) {
     console.log(err);
   } else {
-    console.log('Running on port: ' + app.get('port'));
+    console.log('Running on port: 8080');
+  }
+})
+httpsServer.listen(8443, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Running on port: 8443');
   }
 })
